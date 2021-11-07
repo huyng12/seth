0b1cc25470456002de43c5f3264c2e29746eb8bb
Huy <touch@nguyenrk.com>
2021-11-08 00:56:51 +0700

    Implement Set as Seth contract

---
+++ contracts/Seth.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Seth {
+    struct Set {
+        string[] values;
+        mapping(string => bool) is_contains;
+    }
+
+    Set set;
+    address private contract_owner;
+
+    constructor() {
+        // Who initate the contract will be the owner of the contract
+        contract_owner = msg.sender;
+    }
+
+    function is_contains(string memory s) public view returns (bool) {
+        return set.is_contains[s];
+    }
+
+    function insert(string memory s) public {
+        require(
+            msg.sender == contract_owner,
+            "Only contract owner can insert new string to set"
+        );
+
+        require(
+            !set.is_contains[s],
+            "The string is existing in the set already"
+        );
+
+        set.values.push(s);
+        set.is_contains[s] = true;
+    }
+}
